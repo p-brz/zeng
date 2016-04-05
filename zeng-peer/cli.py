1d5886f2ef422d62a1928125d764219caf8584bc
Paulo Brizolara <souzabrizolara@gmail.com>
2016-04-05 11:02:00 -0300

    Refatoração: extração do tratamento da linha de comando em cli.py

---
+++ zeng-peer/cli.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+from argparse import RawDescriptionHelpFormatter
+
+import os
+from os import path
+import sys
+
+class Cli(object):
+    class Arguments(object):
+        def __init__(self):
+            self.alias = None
+            self.host = None
+            self.dir = os.getcwd()
+
+    class Parser(object):
+        def __init__(self):
+            description = ("Shares a directory with a remote peer or connect with a remote shared folder."
+                    "\n\nEx: "
+                    "\nsteve@hostA {0} . {1}"
+                    "\n\tstart sharing the current dir with the name '{1}'"
+                    "\n"
+                    "\njohn@hostB {0} {1}@{2}"
+                    "\n\tconnect with the remote directory at ip '{2}'").format(sys.argv[0], 'shareddir', '10.0.0.10')
+
+            self.parser = argparse.ArgumentParser(formatter_class=RawDescriptionHelpFormatter
+                , description=description)
+            self.parser.add_argument("dir", nargs='?', default=os.getcwd()
+                , help="the shared directory used by zeng. Defaults to the current dir.")
+            self.parser.add_argument("alias", nargs='?', default=None
+                , help="the name used by peers to connect with the shared folder. Defaults to the directory name")
+
+        def parse(self):
+            args = self.parser.parse_args()
+
+            parts = args.dir.split("@")
+
+            cliArgs = Cli.Arguments()
+
+            if len(parts) >= 2:
+                cliArgs.alias = parts[0]
+                cliArgs.host  = parts[1]
+                cliArgs.dir = os.getcwd()
+            else:
+                cliArgs.dir = path.realpath(args.dir)
+                cliArgs.alias = self._getarg(args, 'alias', path.basename(cliArgs.dir))
+                cliArgs.host = None
+
+            return cliArgs
+
+        def _getarg(self, args, argname, default=None):
+            value = getattr(args, argname, default)
+            return value if value is not None else default
--- zeng-peer/zeng.py
+++ zeng-peer/zeng.py
@@ -1,52 +1,15 @@
 #!/usr/bin/env python
 
-import argparse
-from argparse import RawDescriptionHelpFormatter
-
-import os
-from os import path
-import sys
+from cli import Cli
+from peer import Peer
 
 class Main(object):
-    def __init__(self):
-        description = ("Shares a directory with a remote peer or connect with a remote shared folder."
-                "\n\nEx: "
-                "\nsteve@hostA {0} . {1}"
-                "\n\tstart sharing the current dir with the name '{1}'"
-                "\n"
-                "\njohn@hostB {0} {1}@{2}"
-                "\n\tconnect with the remote directory at ip '{2}'").format(sys.argv[0], 'shareddir', '10.0.0.10')
-
-        self.parser = argparse.ArgumentParser(formatter_class=RawDescriptionHelpFormatter
-            , description=description)
-        self.parser.add_argument("dir", nargs='?', default=os.getcwd()
-            , help="the shared directory used by zeng. Defaults to the current dir.")
-        self.parser.add_argument("alias", nargs='?', default=None
-            , help="the name used by peers to connect with the shared folder. Defaults to the directory name")
-
     def start(self):
-        args = self.parser.parse_args()
+        args = Cli.Parser().parse()
 
-        parts = args.dir.split("@")
-
-        if len(parts) >= 2:
-            self.alias = parts[0]
-            self.host  = parts[1]
-            self.dir = None
-        else:
-            self.dir = path.realpath(args.dir)
-            self.alias = self._get_alias_arg(args, self.dir)
-            self.host = None
-
-        print("host: ", self.host)
-        print("alias: ", self.alias)
-        print("dir: ", self.dir)
-
-    def _get_alias_arg(self, args, share_dir):
-        if args.alias is not None:
-            return args.alias
-        else:
-            return path.basename(share_dir)
+        print("host: ", args.host)
+        print("alias: ", args.alias)
+        print("dir: ", args.dir)
 
 if __name__ == "__main__":
     Main().start()
